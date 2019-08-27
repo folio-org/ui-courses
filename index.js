c7f1b98de09c86f1430a88525366caad6559cd46
Mike Taylor <mike@indexdata.com>
2019-08-27 15:20:36 +0100

    eslint-clean

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default (props) => (
+export default (_props) => (
   <span>
     <h1>This is the Courses app.</h1>
     <p>Maintain courses and reserve items for them.</p>
