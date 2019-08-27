d2c3ccd3982466f83763e46b18526b5bb14a3b35
Mike Taylor <mike@indexdata.com>
2019-08-27 17:01:10 +0100

    First non-trivial version of top-level index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Settings from './settings';
 
-export default (_props) => (
-  <span>
-    <h1>This is the Courses app.</h1>
-    <p>Maintain courses and reserve items for them.</p>
-  </span>
-);
+class CoursesApp extends React.Component {
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    actAs: PropTypes.string.isRequired,
+    stripes: PropTypes.shape({
+      connect: PropTypes.func,
+    }),
+  };
+
+  render() {
+    const {
+      actAs,
+      match: {
+        path
+      }
+    } = this.props;
+
+    if (actAs === 'settings') {
+      return <Settings {...this.props} />;
+    }
+    return (
+      <Switch>
+        <Redirect
+          exact
+          from={path}
+          to={`${path}/courses`}
+        />
+        <Route
+          path={`${path}/courses`}
+          render={() => (
+            <span>
+              <h1>This is the Courses app.</h1>
+              <p>Maintain courses and reserve items for them.</p>
+            </span>
+          )}
+        />
+      </Switch>
+    );
+  }
+}
+
+export default CoursesApp;
