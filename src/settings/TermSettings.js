daa0a0def8e430e889061acc3d3d7d501da574cd
Mike Taylor <mike@indexdata.com>
2019-09-26 13:44:20 +0100

    Fix TermSettings to use date-picker
    At present, to give us working data to run on, it uses the list of
    users instead of the not-yet-available proper endpoint.

--- src/settings/TermSettings.js
+++ src/settings/TermSettings.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
+import { Field, reduxForm } from 'redux-form';
 import { ControlledVocab } from '@folio/stripes/smart-components';
 import { withStripes } from '@folio/stripes/core';
+import { Datepicker } from '@folio/stripes/components';
+
+const fieldComponents = {
+  'expirationDate': ({ fieldProps }) => (
+    <Field
+      {...fieldProps}
+      component={Datepicker}
+      marginBottom0
+      fullWidth
+    />
+  )
+};
 
 class TermSettings extends React.Component {
   static propTypes = {
@@ -25,21 +38,28 @@ class TermSettings extends React.Component {
     return (
       <this.connectedControlledVocab
         stripes={stripes}
-        baseUrl="coursereserves/terms"
+        REAL__baseUrl="coursereserves/terms"
+        baseUrl="users"
+        records="users"
         label={intl.formatMessage({ id: 'ui-courses.objectName.terms' })}
         labelSingular={intl.formatMessage({ id: 'ui-courses.objectName.term' })}
         objectLabel="Entries"
-        visibleFields={['name', 'description']}
+        visibleFields={['username', 'expirationDate']}
+        fieldComponents={fieldComponents}
         columnMapping={{
           name: intl.formatMessage({ id: 'ui-courses.headings.name' }),
           description: intl.formatMessage({ id: 'ui-courses.headings.description' }),
+          startDate: intl.formatMessage({ id: 'ui-courses.headings.startDate' }),
+          endDate: intl.formatMessage({ id: 'ui-courses.headings.endDate' }),
         }}
         id="terms"
-        sortby="name"
+        sortby="username"
         hiddenFields={['lastUpdated', 'numberOfObjects']}
       />
     );
   }
 }
 
-export default injectIntl(withStripes(TermSettings));
+export default reduxForm({
+  form: 'course-reserves-terms'
+})(injectIntl(withStripes(TermSettings)));
