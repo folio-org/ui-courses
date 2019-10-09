4d444bf56b3c2aa9ffc8615dafd04f91673e0a4a
Mike Taylor <mike@indexdata.com>
2019-10-09 17:26:14 +0100

    ViewReserve shows the ID of the reserve

--- src/routes/ViewReserve.js
+++ src/routes/ViewReserve.js
@@ -1,1 +1,16 @@
-export default () => 'This is a single reserve';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ViewReserve = (props) => (
+  <p>This is the single reserve {props.match.params.id}</p>
+);
+
+ViewReserve.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default ViewReserve;
