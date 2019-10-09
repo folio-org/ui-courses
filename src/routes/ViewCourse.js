43f9e449842e2ae98406cd7779ec1c8514ac468f
Mike Taylor <mike@indexdata.com>
2019-10-09 17:22:19 +0100

    ViewCourse shows the ID of the course

--- src/routes/ViewCourse.js
+++ src/routes/ViewCourse.js
@@ -1,1 +1,16 @@
-export default () => 'This is a single course';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ViewCourse = (props) => (
+  <p>This is the single course {props.match.params.id}</p>
+);
+
+ViewCourse.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default ViewCourse;
