dfb8c4c40372d60219ef0ae05466679ae274315e
Mike Taylor <mike@indexdata.com>
2019-10-09 14:16:10 +0100

    Render children

--- src/routes/Courses.js
+++ src/routes/Courses.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Links from './Links';
 
-export default () => (
+export default (props) => (
   <div>
     <p>This is the list of courses</p>
     <Links />
+    {props.children}
   </div>
 );
--- src/routes/Reserves.js
+++ src/routes/Reserves.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Links from './Links';
 
-export default () => (
+export default (props) => (
   <div>
     <p>This is the list of reserves</p>
     <Links />
+    {props.children}
   </div>
 );
