d3b809239636d4c5757f4a3592f6932099e7da7a
Mike Taylor <mike@indexdata.com>
2019-10-09 17:27:42 +0100

    Add PropTypes

--- src/routes/Reserves.js
+++ src/routes/Reserves.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Links from './Links';
 
-export default (props) => (
+const Reserves = (props) => (
   <div>
     <p>This is the list of reserves</p>
     <Links />
     {props.children}
   </div>
 );
+
+Reserves.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export default Reserves;
