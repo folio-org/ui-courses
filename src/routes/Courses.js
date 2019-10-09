e20bafdc7efc57ecef9a4c605956d68b8922a1d3
Mike Taylor <mike@indexdata.com>
2019-10-09 17:25:00 +0100

    Add PropTypes to ViewCourse

--- src/routes/Courses.js
+++ src/routes/Courses.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Links from './Links';
 
-export default (props) => (
+const Courses = (props) => (
   <div>
     <p>This is the list of courses</p>
     <Links />
     {props.children}
   </div>
 );
+
+Courses.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export default Courses;
