994db158fb9e0f57aa38d3b4c8c8fceebf9028cc
Mike Taylor <mike@indexdata.com>
2019-10-09 13:56:56 +0100

    Move base route to /cr/

--- src/routes/Links.js
+++ src/routes/Links.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default () => (
   <ul>
-    <li><Link to="/courses/courses">courses</Link></li>
-    <li><Link to="/courses/courses/123">single course</Link></li>
-    <li><Link to="/courses/reserves">reserves</Link></li>
-    <li><Link to="/courses/reserves/456">single reserve</Link></li>
+    <li><Link to="/cr/courses">courses</Link></li>
+    <li><Link to="/cr/courses/123">single course</Link></li>
+    <li><Link to="/cr/reserves">reserves</Link></li>
+    <li><Link to="/cr/reserves/456">single reserve</Link></li>
   </ul>
 );
