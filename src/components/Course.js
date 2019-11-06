1519d3738be9f236f13d09eb6b141e3f7a7b8dc4
Mike Taylor <mike@indexdata.com>
2019-11-06 18:25:49 +0000

    First (skeletal) full-record display

--- src/components/Course.js
+++ src/components/Course.js
@@ -11,14 +11,75 @@ import {
 import { AppIcon, TitleManager } from '@folio/stripes/core';
 import { Spinner } from '@folio/stripes-erm-components';
 
-const Course = (props) => (
-  <p>This is the single course {props.data.course.id}</p>
-);
+class Course extends React.Component {
+  static propTypes = {
+    data: PropTypes.object,
+    urls: PropTypes.object,
+    isLoading: PropTypes.bool,
+    handlers: PropTypes.shape({
+      onClose: PropTypes.func.isRequired,
+    }).isRequired,
+  };
 
-Course.propTypes = {
-  data: PropTypes.shape({
-    course: PropTypes.object.isRequired,
-  }).isRequired,
-};
+  renderLastMenu = () => {
+    return (
+      <PaneMenu>
+        <FormattedMessage id="ui-courses.editCourse">
+          {ariaLabel => (
+            <Button
+              aria-label={ariaLabel}
+              buttonStyle="primary"
+              id="clickable-edit-course"
+              marginBottom0
+              to={this.props.urls.edit()}
+            >
+              <FormattedMessage id="stripes-components.button.edit" />
+            </Button>
+          )}
+        </FormattedMessage>
+      </PaneMenu>
+    );
+  }
+
+  renderLoadingPane = () => {
+    return (
+      <Pane
+        defaultWidth="fill%"
+        dismissible
+        id="pane-view-course"
+        onClose={this.props.handlers.onClose}
+        paneTitle={<FormattedMessage id="ui-courses.loading" />}
+      >
+        <Layout className="marginTop1">
+          <Spinner />
+        </Layout>
+      </Pane>
+    );
+  }
+
+  render() {
+    const { data, isLoading, handlers } = this.props;
+    const id = (data.course.id ? data.course.id : 'Course ID [Missing]');
+
+    if (isLoading) return this.renderLoadingPane();
+
+    return (
+      <Pane
+        appIcon={<AppIcon app="shipping" />}
+        defaultWidth="fill"
+        dismissible
+        id="pane-view-course"
+        lastMenu={this.renderLastMenu()}
+        onClose={handlers.onClose}
+        paneTitle={'Course ' + id}
+        paneSub="Pane Subtitle"
+      >
+        <TitleManager record={id.substring(0, 8)}>
+          <p>This is the single course {id}</p>
+        </TitleManager>
+      </Pane>
+    );
+  }
+}
 
 export default Course;
