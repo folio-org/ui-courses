58166591ee2dd75dcb381f75f98c4b29406de6d7
Mike Taylor <mike@indexdata.com>
2019-11-08 15:09:01 +0000

    Get actual display elements from new subcomponents

--- src/components/ViewCourse/ViewCourse.js
+++ src/components/ViewCourse/ViewCourse.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ViewCourseSummary from './sections/ViewCourseSummary';
+import ViewCourseDeveloper from './sections/ViewCourseDeveloper';
+
 
 class ViewCourse extends React.Component {
   static propTypes = {
@@ -9,28 +12,11 @@ class ViewCourse extends React.Component {
 
   render() {
     const { record, handlers } = this.props;
-    const departmentObject = record.departmentObject || {};
-    const courseListingObject = record.courseListingObject || {};
-    const termObject = courseListingObject.termObject || {};
-    const courseTypeObject = courseListingObject.courseTypeObject || {};
 
     return (
       <React.Fragment>
-        <ul>
-          <li><b>Name:</b>{record.name}</li>
-          <li><b>Description:</b>{record.description}</li>
-          <li><b>Department:</b>{departmentObject.name}</li>
-          <li><b>Department description:</b>{departmentObject.description}</li>
-          <li><b>Registrar ID:</b>{courseListingObject.registrarId}</li>
-          <li><b>External ID:</b>{courseListingObject.externalId}</li>
-          <li><b>Term:</b>{termObject.name}</li>
-          <li><b>Start date:</b>{termObject.startDate}</li>
-          <li><b>End date:</b>{termObject.endDate}</li>
-          <li><b>Course type:</b>{courseTypeObject.name}</li>
-          <li><b>Course number:</b>{record.courseNumber}</li>
-          <li><b>Section:</b>{record.sectionName}</li>
-        </ul>
-        <pre>{JSON.stringify(record, null, 2)}</pre>
+        <ViewCourseSummary record={record} />
+        <ViewCourseDeveloper record={record} />
       </React.Fragment>
     );
   }
