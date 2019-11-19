9ebc7bd7fb572b64c94a843394aedeac23c6e07d
Mike Taylor <mike@indexdata.com>
2019-11-19 23:17:22 +0000

    Remove entries that are displayed elsewhere

--- src/components/ViewCourse/sections/ViewCourseSummary.js
+++ src/components/ViewCourse/sections/ViewCourseSummary.js
@@ -9,18 +9,9 @@ const ViewCourseSummary = ({ record }) => {
 
   return (
     <ul>
-      <li><b>Name:</b> {record.name}</li>
       <li><b>Description:</b> {record.description}</li>
-      <li><b>Department:</b> {departmentObject.name}</li>
       <li><b>Department description:</b> {departmentObject.description}</li>
-      <li><b>Registrar ID:</b> {courseListingObject.registrarId}</li>
-      <li><b>External ID:</b> {courseListingObject.externalId}</li>
-      <li><b>Term:</b> {termObject.name}</li>
-      <li><b>Start date:</b> {termObject.startDate}</li>
-      <li><b>End date:</b> {termObject.endDate}</li>
       <li><b>Course type:</b> {courseTypeObject.name}</li>
-      <li><b>Course number:</b> {record.courseNumber}</li>
-      <li><b>Section:</b> {record.sectionName}</li>
     </ul>
   );
 };
