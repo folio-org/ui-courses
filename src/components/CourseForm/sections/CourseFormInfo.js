c6bd4361d3e6c943557ed97d5d25706ef44ed841
Mike Taylor <mike@indexdata.com>
2019-12-06 10:10:05 +0000

    Reorganize form, use <TextArea> for description

--- src/components/CourseForm/sections/CourseFormInfo.js
+++ src/components/CourseForm/sections/CourseFormInfo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'react-final-form';
 import { required } from '@folio/stripes-util';
-import { Col, Row, Select, TextField } from '@folio/stripes/components';
+import { Col, Row, Select, TextField, TextArea } from '@folio/stripes/components';
 
 export default class CourseFormInfo extends React.Component {
   static propTypes = {
@@ -36,7 +36,7 @@ export default class CourseFormInfo extends React.Component {
     return (
       <div data-test-course-info id={id}>
         <Row>
-          <Col xs={12}>
+          <Col xs={6}>
             <Field
               component={TextField}
               id="edit-course-name"
@@ -47,20 +47,28 @@ export default class CourseFormInfo extends React.Component {
               validate={required}
             />
           </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
+          <Col xs={6}>
             <Field
-              component={TextField}
-              id="edit-course-description"
-              label={<FormattedMessage id="ui-courses.field.description" />}
-              maxLength={255}
-              name="description"
+              component={Select}
+              dataOptions={data.departments}
+              id="edit-course-department"
+              label={<FormattedMessage id="ui-courses.field.department" />}
+              name="departmentId"
+              required
             />
           </Col>
         </Row>
         <Row>
-          <Col xs={12}>
+          <Col xs={6}>
+            <Field
+              component={TextField}
+              id="edit-course-number"
+              label={<FormattedMessage id="ui-courses.field.number" />}
+              maxLength={255}
+              name="courseNumber"
+            />
+          </Col>
+          <Col xs={6}>
             <Field
               component={TextField}
               id="edit-course-section"
@@ -73,23 +81,11 @@ export default class CourseFormInfo extends React.Component {
         <Row>
           <Col xs={12}>
             <Field
-              component={TextField}
-              id="edit-course-number"
-              label={<FormattedMessage id="ui-courses.field.number" />}
-              maxLength={255}
-              name="courseNumber"
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <Field
-              component={Select}
-              dataOptions={data.departments}
-              id="edit-course-department"
-              label={<FormattedMessage id="ui-courses.field.department" />}
-              name="departmentId"
-              required
+              component={TextArea}
+              id="edit-course-description"
+              label={<FormattedMessage id="ui-courses.field.description" />}
+              name="description"
+              parse={v => v} // Lets us pass an empty string instead of `undefined`
             />
           </Col>
         </Row>
