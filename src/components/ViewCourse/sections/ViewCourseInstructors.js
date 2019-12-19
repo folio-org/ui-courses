a6c64e4b4e9b9dc2e3e5c67c7de57d17413374b9
Mike Taylor <mike@indexdata.com>
2019-12-19 22:26:59 +0000

    Reformat instructors as a table

--- src/components/ViewCourse/sections/ViewCourseInstructors.js
+++ src/components/ViewCourse/sections/ViewCourseInstructors.js
@@ -2,32 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import get from 'lodash/get';
-import { Card, Col, Row, KeyValue } from '@folio/stripes/components';
+import { Card } from '@folio/stripes/components';
 
 const ViewCourseInstructors = ({ record }) => {
   const courseListingObject = record.courseListingObject || {};
   const instructorObjects = courseListingObject.instructorObjects || [];
 
   return (
-    <React.Fragment>
-      {
-        instructorObjects.map((instructor, index) => (
-          <Card key={index} headerStart={`Instructor #${index + 1}`}>
-            <Row>
-              <Col xs={4}>
-                <KeyValue label={<FormattedMessage id="ui-courses.field.instructorName" />} value={instructor.name} />
-              </Col>
-              <Col xs={4}>
-                <KeyValue label={<FormattedMessage id="ui-courses.field.barcode" />} value={instructor.barcode} />
-              </Col>
-              <Col xs={4}>
-                <KeyValue label={<FormattedMessage id="ui-courses.field.patronGroup" />} value={get(instructor, 'patronGroupObject.group')} />
-              </Col>
-            </Row>
-          </Card>
-        ))
-      }
-    </React.Fragment>
+    <Card headerStart="Instructors">
+      <table>
+        <thead>
+          <tr>
+            <th align="left">#</th>
+            <th align="left"><FormattedMessage id="ui-courses.field.instructorName" /></th>
+            <th align="left"><FormattedMessage id="ui-courses.field.barcode" /></th>
+            <th align="left"><FormattedMessage id="ui-courses.field.patronGroup" /></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            instructorObjects.map((instructor, index) => (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{instructor.name}</td>
+                <td>{instructor.barcode}</td>
+                <td>{get(instructor, 'patronGroupObject.group')}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </Card>
   );
 };
 
