dbbacb707c7718b8a296097211b9213a3494bde8
Mike Taylor <mike@indexdata.com>
2020-02-17 18:19:39 +0000

    Update single-reserve link to include clId and cId

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,6 +6,6 @@ export default () => (
     <li><Link to="/cr/courses">courses</Link></li>
     <li><Link to="/cr/courses/123">single course</Link></li>
     <li><Link to="/cr/reserves">reserves</Link></li>
-    <li><Link to="/cr/reserves/456">single reserve</Link></li>
+    <li><Link to="/cr/reserves/123/456/789">single reserve</Link></li>
   </ul>
 );
