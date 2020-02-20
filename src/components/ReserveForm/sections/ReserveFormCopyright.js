9705118012f5ec2abd3e5c225be7ed7ea2d66cf9
Mike Taylor <mike@indexdata.com>
2020-02-20 18:15:44 +0000

    Implement copyright-tracking editing

--- src/components/ReserveForm/sections/ReserveFormCopyright.js
+++ src/components/ReserveForm/sections/ReserveFormCopyright.js
@@ -1,19 +1,99 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import get from 'lodash/get';
-// import { FormattedMessage } from 'react-intl';
-// import { Field } from 'react-final-form';
-import { Col, Row } from '@folio/stripes/components';
+import { FormattedMessage } from 'react-intl';
+import { Field } from 'react-final-form';
+import { Card, Col, Row, TextField, Select, Checkbox } from '@folio/stripes/components';
 
-const ReserveFormCopyright = (_props) => {
+
+const ReserveFormCopyright = (props) => {
+  const { data } = props;
+  // const trackingObject = props.values.copyrightTracking || {};
+  // console.log('ReserveFormCopyright: props =', props);
+
   return (
-    <Row>
-      <Col xs={12}>
-        XXX To do:
-        <tt>copyrightTracking</tt>
-      </Col>
-    </Row>
+    <Card headerStart="Copyright information">
+      {/*
+      <Row>
+        <Col xs={12}>
+          <Field
+            id="edit-reserve-copyright-tracking"
+            name="copyrightTracking."
+            component={TextField}
+            label="Copyright tracking required?"
+          />
+        </Col>
+      </Row>
+      */}
+      <Row>
+        <Col xs={6}>
+          <Field
+            id="edit-reserve-copyright-additional"
+            name="copyrightTracking.additionalSectionsUsed"
+            component={Checkbox}
+            type="checkbox"
+            label={<FormattedMessage id="ui-courses.field.additionalSectionsUsed" />}
+          />
+        </Col>
+        <Col xs={6}>
+          <Field
+            id="edit-reserve-copyright-status"
+            name="copyrightTracking.copyrightStatusId"
+            component={Select}
+            dataOptions={data.copyrightStatuses}
+            label={<FormattedMessage id="ui-courses.field.copyrightStatusId" />}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          <Field
+            id="edit-reserve-copyright-total-pages"
+            name="copyrightTracking.totalPagesInItem"
+            component={TextField}
+            label={<FormattedMessage id="ui-courses.field.totalPagesInItem" />}
+          />
+        </Col>
+        <Col xs={3}>
+          <Field
+            id="edit-reserve-copyright-pages-used"
+            name="copyrightTracking.totalPagesUsed"
+            component={TextField}
+            label={<FormattedMessage id="ui-courses.field.totalPagesUsed" />}
+          />
+        </Col>
+        <Col xs={3}>
+          <Field
+            id="edit-reserve-copyright-percentage-used"
+            name="copyrightTracking.percentOfPages"
+            component={TextField}
+            label={<FormattedMessage id="ui-courses.field.percentOfPages" />}
+          />
+        </Col>
+        <Col xs={3}>
+          <Field
+            id="edit-reserve-copyright-payment-basis"
+            name="copyrightTracking.paymentBasis"
+            component={TextField}
+            label={<FormattedMessage id="ui-courses.field.paymentBasis" />}
+          />
+        </Col>
+      </Row>
+    </Card>
   );
 };
+
+
+ReserveFormCopyright.propTypes = {
+  values: PropTypes.shape({
+    copyrightTracking: PropTypes.shape({
+      copyrightStatusId: PropTypes.string,
+    }),
+  }).isRequired,
+  data: PropTypes.shape({
+    copyrightStatuses: PropTypes.array.isRequired,
+  }).isRequired,
+};
+
 
 export default ReserveFormCopyright;
