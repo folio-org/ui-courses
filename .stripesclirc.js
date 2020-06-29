5d1b0cc514fb62f871fe91520a5eaaa1f42a5baa
Mike Taylor <mike@indexdata.com>
2020-06-29 15:49:23 +0100

    .stripesclircL do not emit logging of NODE_ENV
    Doing so breaks module-descriptor generation, as the emitted
    descriptor begins with a line of debugging output which is not valid
    JSON.

--- .stripesclirc.js
+++ .stripesclirc.js
@@ -1,5 +1,6 @@
-console.log('.stripesclirc.js: NODE_ENV =', process.env.NODE_ENV);
+// console.log('.stripesclirc.js: NODE_ENV =', process.env.NODE_ENV);
 module.exports = {
   okapi: 'https://folio-snapshot-okapi.aws.indexdata.com',
   tenant: 'diku',
+  NODE_ENV: process.env.NODE_ENV,
 };
